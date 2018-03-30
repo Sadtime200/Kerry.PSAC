ef428ac6b0ec8fe48b23c2a183f71ed184230ac6
tony.cai@kerrylogistics.com <tony.cai@kerrylogistics.com>
2018-03-30 11:31:06 +0800

    Add project files.

---
+++ Kerry.PSAC.Data/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kerry.PSAC.Data.Startup))]
+namespace Kerry.PSAC.Data
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
